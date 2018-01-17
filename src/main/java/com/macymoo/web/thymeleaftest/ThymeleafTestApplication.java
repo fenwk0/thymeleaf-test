197fadd1d782f366fabf5433173dea82eeb1df25
Jazmin Fenwick <jazminfenwick@Ruth-fenwicks-iMac.local>
2018-01-17 12:10:41 +0000

    Initial commit

---
+++ src/main/java/com/macymoo/web/thymeleaftest/ThymeleafTestApplication.java
@@ -0,0 +1,12 @@
+package com.macymoo.web.thymeleaftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTestApplication.class, args);
+	}
+}
